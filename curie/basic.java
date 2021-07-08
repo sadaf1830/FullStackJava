c016917f48094721d00cb83c06747753d3693778
sadaf1830 <86623644+sadaf1830@users.noreply.github.com>
2021-07-08 18:34:15 +0530

    Add files via upload

---
+++ curie/basic.java
@@ -0,0 +1,19 @@
+package curie;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//my input data
+		int i=100;
+		int j=200;
+		
+	//processing
+		int s=i+j;
+		
+	//System output
+		System.out.println(s);
+		}
+
+}
---
+++ curie/basic2.java
@@ -0,0 +1,24 @@
+package curie;
+
+import java.util.Scanner;
+
+public class basic2 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter value for i..:");
+		
+		int i=scan.nextInt();
+		
+		System.out.println("Enter value for j..:");
+		
+		int j=scan.nextInt();
+		
+		int sum=i+j;
+		
+		System.out.println("The sum of i and j..:"+sum);
+		
+		}
+
+}
