d21cb4650c97ff78c072c17ba66380abfe59c31e
sadaf1830 <86623644+sadaf1830@users.noreply.github.com>
2021-07-15 02:56:52 +0530

    Add files via upload

---
+++ curie/ArithmeticOperations2.java
@@ -0,0 +1,35 @@
+package basic;
+
+public class ArithmeticOperations2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   //++ - increment operatot i=i+1;
+   //-- - decrement opereator i=i-1;
+		
+		System.out.println("++++++++++++++++++++++++++++++++++++++++");
+		int i=10;
+		System.out.println(i++); //post increment - first the value is printed and incremented
+		System.out.println(i);  //this prints the incremented value
+		
+		int j=100;
+		System.out.println(++j); //pre increment - first the value is incremented then printed
+		System.out.println(j);
+	
+		System.out.println("________________________________________");
+		
+		int ii=10;
+		System.out.println(ii--); //post decrement - first the value is printed then decremented
+		System.out.println(ii); //this prints the decremented value
+		
+		int jj=100;
+		System.out.println(--jj); //pre decrement - first is decremented then printed
+		System.out.println(jj);
+		
+		
+		
+		
+		
+	}
+
+}
